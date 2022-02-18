0da4fbc942f437b865e42b1bab054c8473e5f641
shounakPersistent <98374615+shounakPersistent@users.noreply.github.com>
2022-02-18 14:18:05 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#power function
+x = pow(2,3)
+print (x)
+return (x)
+
+#min() and max() function
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+<<<<<<< HEAD
+#divide ()
+x = x/y           #on master 
+=======
+# Divide 
+ y = x/y            # on branch789
+>>>>>>> a887740 (line 13 Divide function added inmath.py on Bug789)
